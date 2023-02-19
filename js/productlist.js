ddafd048cd41d5b12fedf78e00c45252ed05bc4f
Sophie Riis <sophie.pehrson@gmail.com>
2023-02-19 21:26:30 +0100

    function showAlbum

--- js/productlist.js
+++ js/productlist.js
@@ -6,3 +6,23 @@
 fetch("https://countrysongs-b7f5.restdb.io/rest/country-albums" + id)
   .then((response) => response.json())
   .then((data) => showAlbum(data));
+
+function showAlbums(albums){
+  // looper igennem og kalder showAlbum
+  albums.forEach(showAlbum)
+    
+  }
+
+  function showAlbum(album){
+    console.log(album);
+    //Fange template
+    const template = document.querySelector("regularProductsTemplate").textContent;
+    //Lave en kopi
+    const copy = template.cloneNode(true);
+    //Ændre indhold
+
+    //Appende
+    document.querySelector("main").appendChild(copy);
+  }
+
+ 
