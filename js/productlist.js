c4093cef0bf7d37c67e5899e1cc86d749425c963
Sophie Riis <sophie.pehrson@gmail.com>
2023-02-20 09:26:15 +0100

    restdb data tilføjet

--- js/productlist.js
+++ js/productlist.js
@@ -3,9 +3,10 @@
 // const urlParams = new URLSearchParams(window.location.search);
 // const id = urlParams.get("id");
 
-fetch("https://countrysongs-b7f5.restdb.io/rest/country-albums" + id)
+fetch("https://countrysongs-b7f5.restdb.io/rest/country-albums", //https://countrysongs-b7f5.restdb.io/rest/country-albums
+  {method: "get",headers: {"x-apikey": "63eab3b5478852088da681f2",}}) 
   .then((response) => response.json())
-  .then((data) => showAlbum(data));
+  .then((data) => showAlbums(data));
 
 function showAlbums(albums){
   // looper igennem og kalder showAlbum
@@ -16,14 +17,23 @@ function showAlbums(albums){
   function showAlbum(album){
     console.log(album);
     //Fange template
-    const template = document.querySelector("regularProductsTemplate").textContent;
+    const template = document.querySelector("#regularProductsTemplate").content;
     //Lave en kopi
     const copy = template.cloneNode(true);
     //Ændre indhold
     copy.querySelector("h3").textContent=album.titel;
     //Appende
-    document.querySelector("main").appendChild(copy);
+    document.querySelector(".productlistGrid").appendChild(copy);
   }
+
+// {"_id":"63e9f6fbaa8607500004fa40",
+// "titel":"Southern Rain",
+// "year":2000,
+// "tracks":11,
+// "artist":"Billy Ray Cyrus",
+// "image":"",
+// "spotify":"<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/album/1GO5wJHanTLmWL3Ap3zqpw?utm_source=generator&theme=0\" width=\"100%\" height=\"152\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>",
+// "price":149},
 
  // "Album" i stedet for "Product"
  //Hvordan henter vi vores data fra databasen?
